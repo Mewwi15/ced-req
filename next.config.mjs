8424cbd754772fb490fcf027ef523b08fd7b8df8
Mewwi <pongsakornx15@gmail.com>
2026-04-30 08:21:07 +0700

    fix: add basePath for department server

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // เพิ่มบรรทัดนี้เพื่อบอก Next.js ว่าเรากำลังรันบน sub-path /reqform
+  basePath: "/reqform",
 };
 
 export default nextConfig;
